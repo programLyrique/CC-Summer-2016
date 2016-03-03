d7c3c5dfc4588e75aa1b6d0013b509e5359216ff
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2016-03-03 23:51:17 +0100

    Assignment 1: Hello world

--- selfie.c
+++ selfie.c
@@ -6422,6 +6422,9 @@ int main(int argc, int *argv) {
     argc = argc - 1;
     argv = argv + 1;
 
+    print((int*)"Pierre");
+    println();
+
     if (selfie(argc, (int*) argv) != 0) {
         print(selfieName);
         print((int*) ": usage: selfie { -c source | -o binary | -s assembly | -l binary } [ -m size ... | -d size ... | -y size ... ] ");
