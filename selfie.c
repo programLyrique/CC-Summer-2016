659645ece401b1b9e858873866b8dbe1408b60f5
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2016-03-08 16:37:53 +0100

    From a nominal sentence to a verba sentence...

--- selfie.c
+++ selfie.c
@@ -6422,7 +6422,7 @@ int main(int argc, int *argv) {
     argc = argc - 1;
     argv = argv + 1;
 
-    print((int*)"Pierre");
+    print((int*)"This is Pierre!");
     println();
 
     if (selfie(argc, (int*) argv) != 0) {
